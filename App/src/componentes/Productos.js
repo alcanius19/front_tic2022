6b1147c38c47bb64d7dec26b9f5790133dd20fa0
felipe <felipe_martinezle@fet.edu.co>
2021-10-09 21:44:58 -0500

    formulario de productos

--- App/src/componentes/Productos.js
+++ App/src/componentes/Productos.js
@@ -1,12 +1,76 @@
 import React from "react";
 
-const Productos = () => {
- 
+const Productos = (props) => {
+  const [producto, setProducto] = React.useState(props.producto);
+  // const form = React.useRef(null);
+
+  console.log(producto);
+
+  const [estado, setEstado] = React.useState(false);
+  const enviar = (e) => {
+    e.preventDefault();
+    setProducto(producto);
+    console.log(producto)
+  };
+  const manipularEstado = () => {
+    setEstado(!estado);
+    console.log(estado);
+  };
+
   return (
     <>
-    <div id="title-products" className={"container-fluid bg-ligth"} >
-        <span>Modulo de productos</span>
-    </div>
+      <div id="title-products" className={"container-fluid bg-ligth "}>
+        <h1>Modulo de productos</h1>
+      </div>
+      <hr />
+      <form onSubmit={enviar}>
+        <div className="container">
+          <div className="form-group row p-2">
+            <label className="col-sm-2 col-form-label">Descripcion</label>
+            <div className="col-sm-4 pull-left">
+              <input type="text" name="producto"  />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Valor Unitario"
+              />
+            </div>
+          </div>
+
+          <div className="form-group row p-2">
+            <label className="col-sm-2 col-form-label">Stock</label>
+            <div className="col-sm-4 pull-left">
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Stock"
+              />
+            </div>
+            <div className="col-sm-2">
+              <label>Estado</label>
+            </div>
+            <div className="col-sm-1">
+              <input
+                type="checkbox"
+                value={estado}
+                onChange={manipularEstado}
+              />
+            </div>
+
+            <div className="form-group row p-2">
+              <div className="col-sm-8 "></div>
+              <div className="col-sm-4 ">
+                <button type="submit"   className="btn btn-primary ">
+                  Add Producto
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+      <hr />
     </>
   );
 };
--- App/src/componentes/comun/Pie.js
+++ App/src/componentes/comun/Pie.js
@@ -35,6 +35,7 @@ const Pie = () => {
         </nav>
       </div>
     </div>
+  
   );
 };
 
