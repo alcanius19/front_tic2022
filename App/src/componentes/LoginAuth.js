b20622a38fc7f72b4b83d61bf8ac6d53ff23caac
felipe <felipe_martinezle@fet.edu.co>
2021-10-20 23:43:11 -0500

    modulo administracion de usuarios ok

---
+++ App/src/componentes/LoginAuth.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import axios from "axios";
+
+function LoginAuth() {
+  const [state, setState] = React.useState({
+    isLoggedIn: false,
+    userInfo: {
+      name: "",
+      emailId: "",
+    },
+  });
+
+  const [usuarios, setUsuarios] = React.useState([]);
+
+   const url = "http://localhost:4000/api/usuarios/email";
+
+  const responseSuccessGoogle = (response) => {
+    axios
+      .post("http://localhost:4000/api/usuarios", { tokenId: response.tokenId })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    let userInfo = {
+      name: response.profileObj.name,
+      emailId: response.profileObj.email,
+    };
+    setState({ isLoggedIn: true, userInfo });
+    
+   
+    
+  };
+  
+  const obtenerUsuarios = async () => {
+    let api = url.concat(`/${state.userInfo.emailId}`);
+    console.log(api);
+    const res = await axios.get(api);
+    const users = res.data;
+    setUsuarios(users);
+   
+  };
+  
+  React.useEffect(() =>{
+    setTimeout(() => {
+      if (state.isLoggedIn) {
+         obtenerUsuarios();
+         localStorage.setItem("state", JSON.stringify(usuarios));
+      }else{
+         localStorage.removeItem("state");
+      }
+     
+    },1000)
+  })
+
+
+  const logout = (response) => {
+    console.log(response);
+
+    let userInfo = {
+      name: "",
+      emailId: "",
+    };
+    setState({ isLoggedIn: false, userInfo });
+    localStorage.removeItem('state');
+  };
+  const responseErrorGoogle = (response) => {};
+
+  // <h1>Hola , {state.userInfo.name}</h1>
+  return (
+    <div>
+      {state.isLoggedIn ? (
+        <div>
+          
+          <GoogleLogout
+            clientId="633086225553-495c1chi0spf75vvs1h3j3n3hnfgkufa.apps.googleusercontent.com"
+            buttonText="Logout"
+            onLogoutSuccess={logout}
+          ></GoogleLogout>
+        </div>
+      ) : (
+        <GoogleLogin
+          clientId="633086225553-495c1chi0spf75vvs1h3j3n3hnfgkufa.apps.googleusercontent.com"
+          buttonText="Login with google"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          isSignedIn={true}
+          cookiePolicy={"single_host_origin"}
+        />
+      )}
+    </div>
+  );
+}
+
+export default LoginAuth;
--- App/src/componentes/PaginaUsuarios.js
+++ App/src/componentes/PaginaUsuarios.js
@@ -1,120 +1,161 @@
-//import React, { useEffect, useState } from "react";
 import React from "react";
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import usuariosPrueba from "../ganchos/usuariosPrueba";
+import axios from "axios";
+import { Button, Modal, ModalBody, ModalFooter } from "react-bootstrap";
 
 const PaginaUsuarios = () => {
+  const [modalEditar, setModalEditar] = React.useState(false);
+  const [usuarios, setUsuarios] = React.useState([]);
+  const [estado, setEstado] = React.useState("");
+  const [rol, setRol] = React.useState("");
+  const [usuarioSelect, setUsuarioSelect] = React.useState([
+    {
+      _id: "",
+      estado: "",
+      rol: "",
+    },
+  ]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUsuarioSelect((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(usuarioSelect);
+  };
+
+  const editarUsuario = () => {
+    let api = url.concat(`/${usuarioSelect._id}`);
+    console.log(api);
+    axios.put(api, usuarioSelect).then((response) => {
+      console.log(response);
+    });
+    setModalEditar(false);
+  };
+
+  const seleccionarUsuario = (elemento, caso) => {
+    setUsuarioSelect(elemento);
+    caso === "Editar" && setModalEditar(true);
+  };
+
+  const url = "http://localhost:4000/api/usuarios";
+
+  const obtenerUsuarios = async (url) => {
+    const res = await axios.get(url);
+    const users = res.data;
+    setUsuarios(users);
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      obtenerUsuarios(url);
+    }, 2000);
+  });
 
   return (
     <>
       <div id="title-products" className={"container-fluid bg-ligth "}>
         <h1>Modulo Usuarios</h1>
       </div>
-      <div className="container">
-        <form>
-          <div className="form-group row p-2">
-            <label className="col-sm-2 col-form-label">Nombre</label>
-            <div className="col-sm-4 pull-left">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Nombre"
-              />
-            </div>
-            <div className="col-2">
-              <label>Fecha Ingreso</label>
-            </div>
-            <div className="col-4">
-              <input
-                type="date"
-                className="form-control"
-                placeholder="Fecha Ingreso"
-              />
-            </div>
-          </div>
-
-          <div className="form-group row p-2">
-            <label className="col-sm-2 col-form-label">Cargo</label>
-            <div className="col-sm-4">
-              <input type="text" className="form-control" placeholder="Cargo" />
-            </div>
-            <label className="col-sm-2 col-form-label">Telefono</label>
-            <div className="col-sm-4">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Telefono"
-              />
-            </div>
-            <div className="col-sm-2 mt-2">
-              <label>Estado</label>
-            </div>
-            <div className="col-sm-1 mt-2">
-              <input type="checkbox" checked data-toggle="toggle" />
-            </div>
-
-            <div className="form-group row p-2">
-              <div className="col-sm-8 "></div>
-              <div className="col-sm-4 ">
-                <button className="btn btn-primary">Add Vendedor</button>
-              </div>
-            </div>
-            <hr />
-          </div>
-        </form>
-      </div>
+      <div className="container"></div>
 
       <div className="row justify-content-center">
         <div className="col-lg-10">
           <table id="example" className="table table-responsive">
             <thead>
               <tr>
+                <th>Id</th>
                 <th>Nombre</th>
-                <th>Cargo</th>
-                <th>Telefono</th>
-                <th>Fecha Ingreso</th>
+                <th>Email</th>
+                <th>Rol</th>
                 <th>Estado</th>
+                <th>Fecha</th>
                 <th>Acciones</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Felipe </td>
-                <td>Administrador</td>
-                <td>3223141038</td>
-                <td>22/12/2021</td>
-                <td>Activo</td>
-                <td>
-                  <button className="btn btn-danger">Eliminar</button>
-                  <button className="btn btn-warning">Editar</button>
-                </td>
-              </tr>
-
-              <tr>
-                <td>Jessica </td>
-                <td>Vendedor</td>
-                <td>3223141038</td>
-                <td>10/12/2021</td>
-                <td>Activo</td>
-                <td>
-                  <button className="btn btn-danger">Eliminar</button>
-                  <button className="btn btn-warning">Editar</button>
-                </td>
-              </tr>
-              <tr>
-                <td>Diego </td>
-                <td>Vendedor</td>
-                <td>3223141038</td>
-                <td>02/09/2021</td>
-                <td>Activo</td>
-                <td>
-                  <button className="btn btn-danger ">Eliminar</button>
-                  <button className="btn btn-warning">Editar</button>
-                </td>
-              </tr>
+              {usuarios.map((item, key) => (
+                <tr key={key}>
+                  <td>{key + 1}</td>
+                  <td>{item.nombre}</td>
+                  <td>{item.email}</td>
+                  <td>{item.rol}</td>
+                  <td>{item.estado}</td>
+                  <td>{item.fecha}</td>
+                  <td>
+                    <button className="btn btn-danger">Eliminar</button>{" "}
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => {
+                        seleccionarUsuario(item, "Editar");
+                      }}
+                    >
+                      Editar
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
             <tfoot></tfoot>
           </table>
+
+          <Modal show={modalEditar}>
+            <Modal.Header>
+              <Modal.Title>Editar Usuario</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="form-group">
+                <label>ID</label>
+                <input
+                  className="form-control"
+                  readOnly
+                  type="text"
+                  name="id"
+                  value={usuarioSelect && usuarioSelect._id}
+                  onChange={handleChange}
+                />
+                <br />
+                <label>Estado</label>
+                <select
+                  className="form-control"
+                  name="estado"
+                  value={usuarioSelect && usuarioSelect.estado}
+                  onChange={handleChange}
+                >
+                  <option value={"pendiente"}>Pendiente</option>
+                  <option value={"autorizado"}>Autorizado</option>
+                  <option value={"no Autorizado"}>No Autorizado</option>
+                </select>
+                <br />
+                <label>Rol</label>
+                <select
+                  className="form-control"
+                  name="rol"
+                  value={usuarioSelect && usuarioSelect.rol}
+                  onChange={handleChange}
+                >
+                  <option value={"administrador"}>Admin</option>
+                  <option value={"vendedor"}>Vendedor</option>
+                 
+                </select>
+                <br />
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <button
+                className="btn btn-danger"
+                onClick={() => setModalEditar(false)}
+              >
+                Cancelar
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => editarUsuario()}
+              >
+                Editar
+              </button>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
     </>
